78c23ffc068020eaa38029e448b33577b7de6731
Kengo <funeace@gmail.com>
2020-02-03 16:29:12 +0900

    [Finish]5-5

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,10 @@ Rails.application.routes.draw do
 
   # resource → post_images.id みたいなurlになる。必ず１個になる idが作られない
   # イイね 等
-  resources :post_images, only: [:new, :create, :index, :show] do
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
     resource :favorites, only:[:create,:destroy]
     resource :post_comments, only: [:create]
   end
+
+  resources :users,only: [:show]
 end
